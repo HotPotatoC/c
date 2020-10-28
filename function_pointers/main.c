a65cdf5f60a44e4749a3463cd6a900b5ad0418cf
HotPotatoC <juandotulung@gmail.com>
2020-10-28 19:28:46 +0700

    refactor: removed callback example in function_pointers

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -12,17 +12,6 @@ void multiply(const int x, const int y)
     printf("%d * %d = %d\n", x, y, res);
 }
 
-void baz(const char *msg)
-{
-    printf("%s", msg);
-}
-
-void func_with_callback(void (*cb)(const char *), const char *msg)
-{
-    printf("This is from the callback function\n");
-    (*cb)(msg);
-}
-
 int main(void)
 {
     void (*ptr_foo_func)(int) = foo;
@@ -34,10 +23,6 @@ int main(void)
     ptr_multiply_func(8, 6);
     ptr_multiply_func(10, 10);
     ptr_multiply_func(15, 5);
-
-    void (*cb_func)(const char *) = baz;
-
-    func_with_callback(cb_func, "Hello, World!");
 
     return 0;
 }
