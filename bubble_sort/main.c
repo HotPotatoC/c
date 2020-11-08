e64aea8e43b8e610419479d076fe8f85a01a194c
HotPotatoC <juandotulung@gmail.com>
2020-11-08 14:11:11 +0700

    fix: array length arg

--- bubble_sort/main.c
+++ bubble_sort/main.c
@@ -9,9 +9,9 @@ int main(void)
 {
     int arr[MAX_LENGTH] = {6, 2, 8, 1, 5, 4};
 
-    bubble_sort(arr, MAX_LENGTH - 1);
+    bubble_sort(arr, MAX_LENGTH);
 
-    for (int i = 0; i < MAX_LENGTH - 1; i++)
+    for (int i = 0; i < MAX_LENGTH; i++)
     {
         printf("%d\n", arr[i]);
     }
